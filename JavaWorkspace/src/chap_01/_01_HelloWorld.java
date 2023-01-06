ff929b2eaccb0a07784ff9cb0cd578cb26a0afcf
KBroJ <kbroj9210@gmail.com>
2023-01-06 12:46:44 +0900

    23.01.06_chap_1_HelloWorld/DataTypes

---
+++ JavaWorkspace/src/chap_01/_01_HelloWorld.java
@@ -0,0 +1,11 @@
+package chap_01;
+
+public class _01_HelloWorld {
+
+    public static void main(String[] args) {
+        // Run 단축키 : Shift + F10
+        System.out.println("Hello World!!!");
+        System.out.println("안녕하세요");
+    }
+
+}
---
+++ JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _02_DataTypes {
+    // 메인메소드 단축어 : psvm 또는 main
+    public static void main(String[] args) {
+        // System.out.println(); 단축어 : sout
+        // 1. 문자열 자료형
+        System.out.println("Hello World");
+        System.out.println("안녕하세요?");
+        // 2. 숫자 자료형
+        System.out.println(12);
+        System.out.println(-34);
+        // 3. 불린형(참, 거짓)
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
