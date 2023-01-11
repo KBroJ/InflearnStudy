161b9753763b1d5a82710ed2f04b379e220b8976
KBroJ <kbroj9210@gmail.com>
2023-01-11 12:28:24 +0900

    Operator1_산술연산자_2/2

--- JavaWorkspace/src/chap_02/_01_Operator1.java
+++ JavaWorkspace/src/chap_02/_01_Operator1.java
@@ -35,6 +35,40 @@ public class _01_Operator1 {
         System.out.println(c); // 2
         c = a % b;
         System.out.println(c); // 0
+        System.out.println();
 
+// ====================================================================================
+        // <증감연산>
+        int val;
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(++val); // 10 + 1 => sout 작동
+        System.out.println(val); // 11
+        System.out.println();
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val++); // 10출력 => 11+1
+        System.out.println(val); // 11
+        System.out.println();
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(--val); // 9
+        System.out.println(val); // 9
+        System.out.println();
+
+        val = 10;
+        System.out.println(val); // 10
+        System.out.println(val--); // 10
+        System.out.println(val); // 9
+        System.out.println();
+
+        // 은행 대기번호 표
+        int waiting = 0;
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 0
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 1
+        System.out.println("대기 인원 : " + waiting++); // 대기 인원 : 2
+        System.out.println("총 대기 인원 : " + waiting); // 총 대기 인원 : 3
     }
 }
